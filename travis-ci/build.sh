a55674cbe52e693c8370cbf7776a6e9fdf343397
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2016-08-30 13:16:10 -0400

    Edit build.sh to show error on build error, also set to 64bit player

--- travis-ci/build.sh
+++ travis-ci/build.sh
@@ -9,10 +9,8 @@ echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+  -buildWindows64Player "$(pwd)/Build/windows/$project.exe"
 
 cat $(pwd)/unity.log
