a3827d7689c7b1674e3b37e5286ad0b64eb93abd
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2016-08-30 00:15:51 -0400

    Implement Vive Controller actions using manual UniRx streams

--- Assets/_Scripts/ViveWrapper.cs
+++ Assets/_Scripts/ViveWrapper.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+ Wrapper class for default SteamVR_Controller as well as export of each button/
+ */
+
+using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
 
 public class ViveWrapper : MonoBehaviour {
+	
+	void Start() {
+		//Get SteamVR controllers
+		SteamVR_TrackedObject trackedObject = GetComponent<SteamVR_TrackedObject>();
+		var device = SteamVR_Controller.Input((int)trackedObject.index);
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		//Manual way of doing things
+		this.UpdateAsObservable()
+			.Where(_ => device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+			.Select(_ => device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0))
+			.Subscribe(x => {
+				Debug.Log(x);
+		    }).AddTo(this.gameObject);	
 	}
 }
